c56455e61aa48c6e58058f2ef2362edbe50f321e
José Silva <162442096+josesilvaips@users.noreply.github.com>
2024-04-03 11:47:29 +0100

    Add files via upload

--- AirTransportation.java
+++ AirTransportation.java
@@ -36,12 +36,14 @@ public class AirTransportation extends Transport
     }
     
     public void setNumberOfContainers(int numberOfContainers){
+        if(numberOfContainers > 0){
         this.numberOfContainers = numberOfContainers;
+    }
     }
     
     @Override
     public String getTransportType() {
-        return "Transporte áereo";
+        return "Transporte Aereo";
     }
     
     @Override
@@ -52,12 +54,11 @@ public class AirTransportation extends Transport
         sb.append(String.format("%15s: %s\n", "ID", super.getId()));
         sb.append(String.format("%15s: %s\n", "Origem", super.getOrigin()));
         sb.append(String.format("%15s: %s\n", "Destino", super.getDestination()));
-        sb.append(String.format("%15s: %s\n", "Nome", getName()));
-        sb.append(String.format("%15s: %s\n", "Number of Containters", getNumberOfContainers()));
         sb.append(String.format("%15s: %5.2f€\n", "Preço", super.getPrice()));
         sb.append(String.format("%15s: %4.2f%%\n", "Honorarios", super.getFees()));
         sb.append(String.format("%15s: %4.2f€\n", "Preço Final", super.getPriceWithFees()));
-     
+        sb.append(String.format("%15s: %s\n", "Nome", getName()));
+        sb.append(String.format("%15s: %s\n", "NºContentores", getNumberOfContainers()));
         return sb.toString();
     }
 

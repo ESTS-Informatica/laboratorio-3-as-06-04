084de6b1787dbc83e7b330283b291749f3bd5867
José Silva <162442096+josesilvaips@users.noreply.github.com>
2024-04-03 11:26:16 +0100

    Update GroundTransportation.java

--- GroundTransportation.java
+++ GroundTransportation.java
@@ -7,17 +7,17 @@
  */
 public class GroundTransportation extends Transport
 {
-    // instance variables - replace the example below with your own
     private String licencePlate;
 
     /**
      * Constructor for objects of class GroundTransportation
      */
     public GroundTransportation(String licencePlate)
-    {
+    {       
         this.licencePlate = licencePlate;   
-        setFees(3.0);
+        super.setFees(3.0);
     }
+    
     
     public String getLicensePlate(){
         return this.licencePlate;
@@ -26,5 +26,24 @@ public class GroundTransportation extends Transport
     public void setLicensePlate(String licencePlate){
         this.licencePlate = licencePlate;
     }
-
+    
+    @Override
+    public String getTransportType() {
+        return "Transporte Terrestre";
+    }
+    
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+        sb.append(String.format("\n%15s: %s\n", "Tipo Transporte", getTransportType()));  
+        sb.append(String.format("%15s: %s\n", "ID", super.getId()));
+        sb.append(String.format("%15s: %s\n", "Origem", super.getOrigin()));
+        sb.append(String.format("%15s: %s\n", "Destino", super.getDestination()));
+        sb.append(String.format("%15s: %5.2f€\n", "Preço", super.getPrice()));
+        sb.append(String.format("%15s: %4.2f%%\n", "Honorarios", super.getFees()));
+        sb.append(String.format("%15s: %4.2f€\n", "Preço Final", super.getPriceWithFees()));
+        sb.append(String.format("%15s: %s\n", "Matricula", this.getLicensePlate()));
+     
+        return sb.toString();
+    }
 }
